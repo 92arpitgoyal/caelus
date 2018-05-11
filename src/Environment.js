9297d559878dde11aac71a346739f91938c2e2e5
Arpit Goyal <arpit.goyal@decurtis.com>
2018-05-11 12:24:49 +0530

    update ship and shore environemnts for DXP and VV

--- src/Environment.js
+++ src/Environment.js
@@ -1,25 +1,37 @@
 /* Different URLs for different environments */
 
 /***** VV Dev Ship *****/
-// export const ENVIRONMENT = 'VV DEV Ship';
-// export const PORT = '10.98.0.85:4984';
+export const ENVIRONMENT = 'VV DEV Ship';
+export const PORT = 'vvship.d-aws-vxp.com/dev-syncgateway';
+export const USERNAME = 'vvdev'
+export const PASSWORD = 'yellow*99'
+
+/***** VV Dev Shore *****/
+// export const ENVIRONMENT = 'VV DEV Shore';
+// export const PORT = '10.3.100.115:4984';
 // export const USERNAME = 'vvdev'
 // export const PASSWORD = 'yellow*99'
 
 /***** VV QC Ship *****/
 // export const ENVIRONMENT = 'VV QC Ship'
-// export const PORT = '10.99.0.189:4984';
+// export const PORT = 'vvship.d-aws-vxp.com/integration-syncgateway';
+// export const USERNAME = 'decurtis'
+// export const PASSWORD = 'pass'
+
+/***** VV QC Shore *****/
+// export const ENVIRONMENT = 'VV QC Shore';
+// export const PORT = '10.4.0.112:4984';
 // export const USERNAME = 'decurtis'
 // export const PASSWORD = 'pass'
 
 /***** DXP Demo Ship *****/
-export const ENVIRONMENT = 'DXP Demo Ship'
-export const PORT = '34.236.78.48:4984';
-export const USERNAME = 'decurtis'
-export const PASSWORD = 'pass'
+// export const ENVIRONMENT = 'DXP Demo Ship'
+// export const PORT = 'dxpship.decurtiscorp.com/syncgateway';
+// export const USERNAME = 'decurtis'
+// export const PASSWORD = 'pass'
 
 /***** DXP Demo Shore *****/
 // export const ENVIRONMENT = 'DXP Demo Shore'
-// export const PORT = '34.236.64.254:4984';
+// export const PORT = 'dxpshore.decurtiscorp.com/syncgateway';
 // export const USERNAME = 'decurtis'
 // export const PASSWORD = 'pass'
