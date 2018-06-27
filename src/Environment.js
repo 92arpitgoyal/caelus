616a2afc78db7fd2f85a75f6a4b863d746f4be81
Arpit Goyal <arpit.goyal@decurtis.com>
2018-06-27 12:03:19 +0530

    update http to https urls

--- src/Environment.js
+++ src/Environment.js
@@ -2,7 +2,7 @@
 
 /***** VV Dev Ship *****/
 export const ENVIRONMENT = 'VV DEV Ship';
-export const PROTOCOL = 'http'
+export const PROTOCOL = 'https'
 export const PORT = 'vvship.d-aws-vxp.com/dev-syncgateway';
 export const USERNAME = 'vvdev'
 export const PASSWORD = 'yellow*99'
@@ -16,7 +16,7 @@ export const PASSWORD = 'yellow*99'
 
 /***** VV QC Ship *****/
 // export const ENVIRONMENT = 'VV QC Ship'
-// export const PROTOCOL = 'http'
+// export const PROTOCOL = 'https'
 // export const PORT = 'vvship.d-aws-vxp.com/integration-syncgateway';
 // export const USERNAME = 'decurtis'
 // export const PASSWORD = 'pass'
@@ -30,14 +30,14 @@ export const PASSWORD = 'yellow*99'
 
 /***** DXP Demo Ship *****/
 // export const ENVIRONMENT = 'DXP Demo Ship'
-// export const PROTOCOL = 'http'
+// export const PROTOCOL = 'https'
 // export const PORT = 'dxpship.decurtiscorp.com/syncgateway';
 // export const USERNAME = 'decurtis'
 // export const PASSWORD = 'pass'
 
 /***** DXP Demo Shore *****/
 // export const ENVIRONMENT = 'DXP Demo Shore'
-// export const PROTOCOL = 'http'
+// export const PROTOCOL = 'https'
 // export const PORT = 'dxpshore.decurtiscorp.com/syncgateway';
 // export const USERNAME = 'decurtis'
 // export const PASSWORD = 'pass'
