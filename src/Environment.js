a8bab6759185d676e9e10ebc8d40c11ce73bbd84
Arpit Goyal <arpit.goyal@decurtis.com>
2018-10-03 13:07:15 +0530

    keep only shore URLs and add paths for testing

--- src/Environment.js
+++ src/Environment.js
@@ -1,27 +1,11 @@
 /* Different URLs for different environments */
 
-/***** VV Dev Ship *****/
-// export const ENVIRONMENT = 'VV DEV Ship';
-// export const PROTOCOL = 'https'
-// export const BASE = 'vvship.d-aws-vxp.com/dev-syncgateway';
-// export const BUCKET_NAME = 'appdocstorenew_v1'
-// export const USERNAME = 'vvdevappdocstorenew_v1'
-// export const PASSWORD = 'yellow*99'
-
 /***** VV Dev Shore *****/
 // export const ENVIRONMENT = 'VV DEV Shore';
 // export const PROTOCOL = 'https'
 // export const BASE = 'vvshore-dev.d-aws-vxp.com/syncgateway';
 // export const BUCKET_NAME = 'appdocstorenew_v1'
 // export const USERNAME = 'vvdevappdocstorenew_v1'
-// export const PASSWORD = 'yellow*99'
-
-/***** VV QC Ship *****/
-// export const ENVIRONMENT = 'VV QC Ship'
-// export const PROTOCOL = 'https'
-// export const BASE = 'vvship.d-aws-vxp.com/integration-syncgateway';
-// export const BUCKET_NAME = 'appdocstorenew_v1'
-// export const USERNAME = 'vvqcappdocstorenew_v1'
 // export const PASSWORD = 'yellow*99'
 
 /***** VV QC Shore *****/
@@ -31,14 +15,6 @@
 // export const BUCKET_NAME = 'appdocstorenew_v1'
 // export const USERNAME = 'vvqcappdocstorenew_v1'
 // export const PASSWORD = 'yellow*99'
-
-/***** DXP Demo Ship *****/
-export const ENVIRONMENT = 'DXP Demo Ship'
-export const PROTOCOL = 'https'
-export const BASE = 'dxpship.decurtiscorp.com/syncgateway';
-export const BUCKET_NAME = 'prodappdocstore'
-export const USERNAME = 'dxpprodappdocstore'
-export const PASSWORD = 'yellow*99'
 
 /***** DXP Demo Shore *****/
 // export const ENVIRONMENT = 'DXP Demo Shore'
