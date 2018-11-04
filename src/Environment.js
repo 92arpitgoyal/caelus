20912a4cb37a54c3f827aee929e8744f6d3f4d3f
Arpit Goyal <92arpit.goyal@gmail.com>
2018-11-04 12:11:28 +0530

    Update Environment.js

--- src/Environment.js
+++ src/Environment.js
@@ -1,25 +1,9 @@
 /* Different URLs for different environments */
 
-/***** VV Dev Shore *****/
-// export const ENVIRONMENT = 'VV DEV Shore';
+/***** Dummy Path *****/
+// export const ENVIRONMENT = 'DEV';
 // export const PROTOCOL = 'https'
-// export const BASE = 'vvshore-dev.d-aws-vxp.com/syncgateway';
-// export const BUCKET_NAME = 'appdocstorenew_v1'
-// export const USERNAME = 'vvdevappdocstorenew_v1'
-// export const PASSWORD = 'yellow*99'
-
-/***** VV QC Shore *****/
-// export const ENVIRONMENT = 'VV QC Shore';
-// export const PROTOCOL = 'https'
-// export const BASE = 'vvshore-integration.d-aws-vxp.com/syncgateway';
-// export const BUCKET_NAME = 'appdocstorenew_v1'
-// export const USERNAME = 'vvqcappdocstorenew_v1'
-// export const PASSWORD = 'yellow*99'
-
-/***** DXP Demo Shore *****/
-// export const ENVIRONMENT = 'DXP Demo Shore'
-// export const PROTOCOL = 'https'
-// export const BASE = 'dxpshore.decurtiscorp.com/syncgateway';
-// export const BUCKET_NAME = 'prodappdocstore'
-// export const USERNAME = 'dxpprodappdocstore'
-// export const PASSWORD = 'yellow*99'
+// export const BASE = 'dev.com/syncgateway';
+// export const BUCKET_NAME = 'new_v1'
+// export const USERNAME = 'new_v1'
+// export const PASSWORD = 'yellow*99green'
